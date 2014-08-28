e5d669619cf2b3d4d48515315daa3444a849b3c9
Pasha <by.oops@mail.ru>
2014-08-28 14:25:38 +0300

    fixed column

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -1,23 +1,23 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
 
-<?php if (isset($this->breadcrumbs)): ?>
-    <ol class="breadcrumb">
-        <?php foreach ($this->breadcrumbs as $value): ?>
-            <li><a href="#"><?=$value?></a></li>
-        <?php endforeach; ?>
-    </ol>
-<?php endif; ?>
-
-<div class="span-19">
+<div class="col-md-9">
 	<div id="content">
-		<ol class="breadcrumb">
-  			<li><a href="#">Home</a></li>
-		</ol>
-		<?php echo $content; ?>
+        <?php if (isset($this->breadcrumbs)): ?>
+            <ol class="breadcrumb">
+                <li><a href="#">
+                        <i class="fa fa-home"></i>
+                    </a></li>
+                <?php foreach ($this->breadcrumbs as $value): ?>
+                    <li><a href="#"><?=$value?></a></li>
+                <?php endforeach; ?>
+            </ol>
+        <?php endif; ?>
+
+        <?php echo $content; ?>
 	</div><!-- content -->
 </div>
-<div class="span-5 last">
+<div class="col-md-3">
 	<div id="sidebar">
 	<?php
 		$this->beginWidget('zii.widgets.CPortlet', array(
