c7ba80a132e098c5febb8a4723ef6eec4a6cbedb
metallica <diekey@mail.ru>
2014-08-29 23:53:21 +0300

    init AdminController + swap columns

---
+++ protected/components/AdminController.php
@@ -0,0 +1,5 @@
+<?php
+	class AdminController extends Controller
+	{
+		public $layout='//layouts/column2';
+	}
--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -2,22 +2,6 @@
 	<?php /* @var $this Controller */ ?>
 	<?php $this->beginContent('//layouts/main'); ?>
 <div class="row">
-	<div class="col-md-9">
-		<div id="content">
-	        <?php if (isset($this->breadcrumbs)): ?>
-	            <ol class="breadcrumb">
-	                <li><a href="#">
-	                        <i class="fa fa-home"></i>
-	                    </a></li>
-	                <?php foreach ($this->breadcrumbs as $value): ?>
-	                    <li><a href="#"><?=$value?></a></li>
-	                <?php endforeach; ?>
-	            </ol>
-	        <?php endif; ?>
-
-	        <?php echo $content; ?>
-		</div><!-- content -->
-	</div>
 	<div class="col-md-3">
 		<div id="sidebar">
 		<?php
@@ -31,6 +15,22 @@
 			$this->endWidget();
 		?>
 		</div><!-- sidebar -->
+	</div>
+	<div class="col-md-9">
+		<div id="content">
+			<?php if (isset($this->breadcrumbs)): ?>
+				<ol class="breadcrumb">
+					<li><a href="#">
+							<i class="fa fa-home"></i>
+						</a></li>
+					<?php foreach ($this->breadcrumbs as $value): ?>
+						<li><a href="#"><?=$value?></a></li>
+					<?php endforeach; ?>
+				</ol>
+			<?php endif; ?>
+
+			<?php echo $content; ?>
+		</div><!-- content -->
 	</div>
 </div>
 	<?php $this->endContent(); ?>
