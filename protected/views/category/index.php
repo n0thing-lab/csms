f260d7066780482859005831649f87e734062d20
Pasha <by.oops@mail.ru>
2014-08-31 17:24:41 +0300

    fix visota knpoki(button)

--- protected/views/category/index.php
+++ protected/views/category/index.php
@@ -10,7 +10,7 @@ $this->breadcrumbs=array(
 
 <h1><?=$this->pageTitle?></h1>
 
-<div class="row">
+<div class="row" style="width:100%; min-height: 270px;">
 	<div class="add-button col-md-3 text-center">
 		<a href="#" class="text-success">
 			<i class="fa fa-plus-square fa-5x"></i>
@@ -26,29 +26,13 @@ $this->breadcrumbs=array(
             </a>
         </div>
     <?php endforeach;?>
-    <div class="col-md-3">
-        <a href="#" class="thumbnail">
-            <img style="width:200px; height:200px">
-            <h4 class="text-center">da</h4>
-        </a>
-    </div>
-    <div class="col-md-3">
-        <a href="#" class="thumbnail">
-            <img style="width:200px; height:200px">
-            <h4 class="text-center">da</h4>
-        </a>
-    </div>
-    <div class="col-md-3">
-        <a href="#" class="thumbnail">
-            <img style="width:200px; height:200px">
-            <h4 class="text-center">da</h4>
-        </a>
-    </div>
+
 </div>
 
 <script>
 $(document).ready(function(){
-	var height = $('.add-button').parent().find('div.col-md-3:nth-child(2)').find('img').height();
+	var height = 269;
+        //$('.add-button').parent().find('div.col-md-3:nth-child(2)').find('img').height();
 	var h = $('.add-button').height();
 
 	$('.add-button i').css({'margin-top':(height-h)/2});
