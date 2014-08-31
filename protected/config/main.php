b858f871d1e49ffa1b7852385edf1d838d7e5f46
v0lume <heads@xakep.ru>
2014-09-01 01:11:00 +0300

    clearing

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Yiimine',
+	'name'=>'ЦСМС Документы',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
