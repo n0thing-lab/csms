efda216bfaad4ae5418d30c4e8fc1e8c7d93008b
metallica <diekey@mail.ru>
2014-09-21 23:14:52 +0300

    fix create categoryAdmin form

--- protected/views/categoryAdmin/_form.php
+++ protected/views/categoryAdmin/_form.php
@@ -4,7 +4,7 @@
 /* @var $form CActiveForm */
 ?>
 
-<div class="form">
+<div class="form-horizontal">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'category-form',
@@ -19,30 +19,39 @@
 
 	<?php echo $form->errorSummary($model); ?>
 
-    <div class="row">
-        <?php
-        $this->widget('ext.yii-image-attachment.ImageAttachmentWidget', array(
-        'model' => $model,
-        'behaviorName' => 'cover',
-        'apiRoute' => 'categoryAdmin/saveImageAttachment',
-        ));
-        ?>
-    </div>
+		<div class="form-group">
+			<div class="col-sm-10">
+			<?php
+				$this->widget('ext.yii-image-attachment.ImageAttachmentWidget', array(
+					'model' => $model,
+					'behaviorName' => 'cover',
+					'apiRoute' => 'categoryAdmin/saveImageAttachment',
+				));
+			?>
+			</div>
+		</div>
 
-	<div class="row">
-        <?php echo $form->labelEx($model,'parent'); ?>
-        <?php echo $form->dropDownList($model, 'parent', CHtml::listData(Category::model()->findAll(), 'id', 'name'), array('prompt' =>'Выберите категорию')); ?>
-        <?php echo $form->error($model,'parent'); ?>
-    </div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'name'); ?>
-		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'name'); ?>
+	<div class="form-group">
+		<?php echo $form->labelEx($model,'parent', array('class'=>"col-sm-2 control-label")); ?>
+		<div class="col-sm-10">
+            <?php echo $form->dropDownList($model, 'parent',
+	            CHtml::listData(Category::model()->findAll(), 'id', 'name'), array('prompt' =>'Выберите категорию', 'class'=>"form-control")); ?>
+            <?php echo $form->error($model,'parent'); ?>
+		</div>
 	</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	<div class="form-group">
+		<?php echo $form->labelEx($model,'name', array('class'=>"col-sm-2 control-label")); ?>
+		<div class="col-sm-10">
+			<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>255, 'class'=>"form-control", "placeholder"=>"Name")); ?>
+			<?php echo $form->error($model,'name'); ?>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class'=>"btn btn-default")); ?>
+		</div>
 	</div>
 
 <?php $this->endWidget(); ?>
