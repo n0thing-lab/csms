1204d14d52007e29c8ff53f3ac6e8e4cc2c5a094
Pasha <by.oops@mail.ru>
2014-09-24 23:48:31 +0300

    fix login

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -12,13 +12,34 @@ class UserController extends Controller
 	{
         $model = new LoginForm();
 
+        if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
+        {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+
         if (isset($_POST['LoginForm']))
         {
             $model->attributes = $_POST['LoginForm'];
+            if($model->validate() && $model->login())
+                $this->redirect(Yii::app()->user->returnUrl);
         }
 
         $this->layout = '//layouts/login';
         $this->render('login', array('model'=>$model));
+
+        //--------------
+
+        $model=new LoginForm;
+
+        // if it is ajax validation request
+        if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
+        {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+
+        // collect user input data
 	}
 
 	// Uncomment the following methods and override them if needed
