0de1399aa68bd39a4b8bebbc94942a34e7616f52
metallica <diekey@mail.ru>
2014-10-26 14:59:22 +0200

    create crud for documents and delete from it index action with view

---
+++ protected/views/document/_view.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $this DocumentController */
+/* @var $data Document */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('category_id')); ?>:</b>
+	<?php echo CHtml::encode($data->category_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('year')); ?>:</b>
+	<?php echo CHtml::encode($data->year); ?>
+	<br />
+
+
+</div>
